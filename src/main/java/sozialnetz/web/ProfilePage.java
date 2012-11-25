8153949af4585922ffc07ca20292a88f14e06d70
Chino <joseignaciosg9@gmail.com>
2012-11-25 19:25:30 -0300

    se llega hasta la profile page logueandose

--- src/main/java/sozialnetz/web/ProfilePage.java
+++ src/main/java/sozialnetz/web/ProfilePage.java
@@ -1,12 +1,13 @@
 package sozialnetz.web;
 
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
 import sozialnetz.web.base.BasePage;
 
 public class ProfilePage extends BasePage {
 
 	private static final long serialVersionUID = 1L;
 	
-	public ProfilePage(){
-		
+	public ProfilePage(PageParameters parameters){
 	}
 }
--- src/main/java/sozialnetz/web/base/NotLoggedHeaderPanel.java
+++ src/main/java/sozialnetz/web/base/NotLoggedHeaderPanel.java
@@ -4,18 +4,18 @@ import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import sozialnetz.domain.repositories.api.UserRepo;
 import sozialnetz.web.ProfilePage;
 import sozialnetz.web.SozialneztSession;
 
-
 public class NotLoggedHeaderPanel extends Panel {
 
 	@SpringBean
 	private UserRepo userRepo;
-	
+
 	private String username;
 	private String password;
 
@@ -23,18 +23,19 @@ public class NotLoggedHeaderPanel extends Panel {
 
 	public NotLoggedHeaderPanel(String id) {
 		super(id);
-		//login form
-		Form<NotLoggedHeaderPanel> loginform = new Form<NotLoggedHeaderPanel>("loginform",
-				new CompoundPropertyModel<NotLoggedHeaderPanel>(this)){
+		// login form
+		Form<NotLoggedHeaderPanel> loginform = new Form<NotLoggedHeaderPanel>(
+				"loginform", new CompoundPropertyModel<NotLoggedHeaderPanel>(
+						this)) {
 			@Override
-			public void onSubmit(){
+			public void onSubmit() {
 				SozialneztSession session = SozialneztSession.get();
-				if (session.signIn(username, password,userRepo)){
-					if(!continueToOriginalDestination()){ //TODO TEST THIS
-						setResponsePage(new ProfilePage());
-					}else{
-						error(getString("invalidLogin"));//TODO TEST SHIT
+				if (session.signIn(username, password, userRepo)) {
+					if (!continueToOriginalDestination()) {
+						setResponsePage(new ProfilePage(new PageParameters().add("username", username)));
 					}
+				} else {
+					error(getString("invalidCredentials"));
 				}
 			}
 		};
